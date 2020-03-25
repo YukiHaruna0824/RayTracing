da5658bc0035135b24bfb1190b6882e8a749e52a
Jason Zhang <z3010019@gmail.com>
2020-03-25 22:49:18 +0800

    add cpbrt parser

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleFileBrowser;
+
+public class UIManager : MonoBehaviour
+{
+    private string _rootFolder;
+    private Parser _parser = new Parser();
+
+    public void OpenFileBrowser()
+    {
+        FileBrowser.AddQuickLink("Desktop", "C:\\Users\\Desktop", null);
+        StartCoroutine(ShowLoadDialogCorotine());
+    }
+
+
+    IEnumerator ShowLoadDialogCorotine()
+    {
+        yield return FileBrowser.WaitForLoadDialog(false, null, "Select Folder", "Select");
+        _rootFolder = FileBrowser.Result;
+        _parser.Parse(_rootFolder);
+    }
+}
